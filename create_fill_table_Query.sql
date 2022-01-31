5743f7116570cc6ca7bb798b06f04fff5aa08100
afnan-m <73725776+afnan-m@users.noreply.github.com>
2022-01-31 06:57:30 +0300

    Add files via upload
    This project is based on Azure cloud services. 
    
    1) resourceAutomation file is used to create the necessary resources:
    -Resource group, 
    -Webapp and its server,
    - SQL Server, firewall rules, and database 
    - Connection for webapp and databse
    
    2) create_fill_table_Query file includes a script to create an empty table on the SQL database as well as an insert script. This script was executed using the query editor on the database inside the Azure portal.
    
    3) table_view_query file includes a single line code to be executed in the query editor to view the entries table.
    
    4) QAfinalDatabse and friendlypersona files were used to create the knowledge base and the chit-chat personality of the chat bot using QnA maker from Azure services.
    
    5) chatBot_code file includes the embedded source code of the chatbot.
    
    6) websiteSourceCode file includes the html source code to create the website and it was created using Visual Studio then published to the Azure webapp.
    
    Additionally, a load balancer and virtual machines along with the required Virtual Network,  Public IP, NAT Gateway, Network Security Group, and other necessary resources were created using Azure portal interface.

---
+++ create_fill_table_Query.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [dbo].[academic_calendar](
+	[Date] [varchar](Max) NOT NULL,
+	[Event] [varchar](Max) NOT NULL,
+	
+
+    )
+
+insert into academic_calendar(Date, Event) values('7 March','Spring semester begins');
+insert into academic_calendar(Date, Event) values('14-16 March','Add/Drop period');
+insert into academic_calendar(Date, Event) values('22 April','Deadline for leave of absence');
+insert into academic_calendar(Date, Event) values('23 April','Holiday (National Sovereignty and the Children’s Day )');
+insert into academic_calendar(Date, Event) values('24 April - 6 May','Midterm examinations');
+insert into academic_calendar(Date, Event) values('1 May','Holiday (Labor and Solidarity Day)');
+insert into academic_calendar(Date, Event) values('2 May - 4 May','Holiday (Ramadan)');
+insert into academic_calendar(Date, Event) values('19 May','Holiday (The Commemoration of Ataturk, Youth and Sports Day)');
+insert into academic_calendar(Date, Event) values('20 May','Withdrawal deadline');
+insert into academic_calendar(Date, Event) values('7 June','Istanbul Bilgi University Foundation Day');
+insert into academic_calendar(Date, Event) values('17 June','End of spring semester classes');
+insert into academic_calendar(Date, Event) values('21 June - 3 July','Spring semester final examinations');
+insert into academic_calendar(Date, Event) values('6 July','Submission of grades');
---
+++ resourceAutomation.ps1
@@ -0,0 +1,46 @@
+﻿Connect-AzAccount
+
+# required variables
+
+$resourceName = "Final_Project"
+$location = "West US"
+$WebAppName_server= "FPApp_server"
+$WebAppName= "finalproject-webapp"
+
+$adminSqlLogin = "cmpe363"
+$password = "Meca1234"
+
+#$MysubscriptionId = ''
+Set-AzContext -SubscriptionId 88e623fd-6077-48f4-996d-b149cbf93006
+
+$serverName = "finalproject-cmpe363"
+$databaseName = "DB_FinalProject"
+
+
+#create a resource group
+
+New-AzResourceGroup -Name $resourceName -Location $location
+
+
+#create web app and service
+
+
+New-AzAppServicePlan -Name $WebAppName_server -Location $location -ResourceGroupName $resourceName -Tier D1
+New-AzWebApp -Name $WebAppName -Location $location -AppServicePlan $WebAppName_server -ResourceGroupName $resourceName
+
+
+#create SQL database server &  firewall rules
+
+New-AzSqlServer -ResourceGroupName $resourceName ` -ServerName $serverName ` -Location $location ` -SqlAdministratorCredentials $(New-Object -TypeName System.Management.Automation.PSCredential -ArgumentList $adminSqlLogin, $(ConvertTo-SecureString -String $password -AsPlainText -Force))
+New-AzSqlServerFirewallRule -ResourceGroupName $resourceName ` -ServerName $serverName ` -FirewallRuleName "AllowedIPs" -StartIpAddress 0.0.0.0 -EndIpAddress 0.0.0.0
+
+#create empty SQL database
+
+New-AzSqlDatabase  -ResourceGroupName $resourceName ` -ServerName $serverName ` -DatabaseName $databaseName ` -RequestedServiceObjectiveName "S0"
+
+#connect database with the web app
+
+Set-AzWebApp -ConnectionStrings @{ ConnectionString = @{ Type="SQLAzure"; Value ="Server=tcp:$serverName.database.windows.net;Database=$databaseName;User ID=$adminSqlLogin@$ServerName;Password=$password;Trusted_Connection=False;Encrypt=True;" } } -Name $WebAppName -ResourceGroupName $resourceName
+
+
+
